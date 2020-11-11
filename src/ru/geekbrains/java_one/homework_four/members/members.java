faea5893bc6ca9f2b739e366b8e179231a932920
BIS84 <bbiiss84@mail.ru>
2020-11-12 02:39:51 +0300

    add task 2

--- src/ru/geekbrains/java_one/homework_four/members/members.java
+++ src/ru/geekbrains/java_one/homework_four/members/members.java
@@ -8,4 +8,12 @@ public class members {
     int phone;
     float salary;
     int age;
+
+    public members(String fullName, String position, int phone, float salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 }

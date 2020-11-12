74805b9f02884d0f6c99fc3ced4f0f2335ba45fe
BIS84 <bbiiss84@mail.ru>
2020-11-12 05:51:44 +0300

    add task 5

--- src/ru/geekbrains/java_one/homework_four/Main.java
+++ src/ru/geekbrains/java_one/homework_four/Main.java
@@ -6,7 +6,26 @@ public class Main {
 
     public  static void main(String[] args) {
         // Не понятно чьи ФИО и должность вывести, ведь у нас еще нет ни одного сотрудника.
-        members member1 = new members("Ivanov Ivan Ivanovich", "director", 11111111111l, 2000000, 55);
-        System.out.println(member1.getFullName() + " - " + member1.getPosition());
+        members member0 = new members("Ivanov Ivan Ivanovich", "director", 11111111111l, 2000000, 55);
+        System.out.println(member0.getFullName() + " - " + member0.getPosition());
+        System.out.println();
+
+        members member1 = new members("name1", "position1", 22222222222l, 50000, 25);
+        members member2 = new members("name2", "position2", 33333333333l, 32000, 41);
+        members member3 = new members("name3", "position3", 44444444444l, 44000, 55);
+        members member4 = new members("name4", "position4", 55555555555l, 25000, 47);
+        members member5 = new members("name5", "position5", 66666666666l, 40000, 30);
+        members[] Members = {member1, member2, member3, member4, member5};
+        for (int i = 0; i < 5; i++) {
+            members member = Members[i];
+            if (member.getAge() > 40) {
+                System.out.println(member.getFullName());
+                System.out.println(member.getPosition());
+                System.out.println(member.getPhone());
+                System.out.println(member.getSalary());
+                System.out.println(member.getAge());
+                System.out.println();
+            }
+        }
     }
 }
